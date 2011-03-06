78206120094aaf0fd835b6af98cbbd0a4ba80729
balhar.jakub <balhar.jakub@gmail.com>
2011-03-06 10:25:41 +0000

    Uprava kvuli escapovani " na strane serveru

--- tools/ARDesigner/testSetInfo.php
+++ tools/ARDesigner/testSetInfo.php
@@ -1,14 +1,17 @@
 <?php
+    require_once('sources/models/JSON.php');
     require_once('sources/models/serializeRules/AncestorSerializeRules.php');
     require_once('sources/models/serializeRules/SerializeRulesBackgroundAssociationRules.php');
     require_once('sources/models/serializeRules/SerializeRulesARQuery.php');
     require_once('sources/models/serializeRules/SerializeRulesTaskSetting.php');
     require_once('sources/models/serializeRules/SerializeRulesQueryByAR.php');
+    require_once('sources/models/Utils.php');
 
     session_start();
-    
+
     $toSolve = $_POST['data'];
-    
+    $toSolve = str_replace("\\\"", "\"", $toSolve);
+
     //$sr = new SerializeRulesBackgroundAssociationRules();
     //$sr = new SerializeRulesTaskSetting();
     //$sr = new SerializeRulesARQuery();
