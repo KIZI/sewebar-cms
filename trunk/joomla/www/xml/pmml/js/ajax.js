049b57d7adecf4809c365328c55b52a979e65344
kliegr@gmail.com <kliegr@gmail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2011-03-15 09:49:58 +0000

    xml replaces specifications

---
+++ trunk/joomla/www/xml/pmml/js/ajax.js
@@ -0,0 +1,31 @@
+//from: components/com_kbi/assets/js.js
+
+function KbiPostArb(id, src_type, query_type)
+{
+	var service_url = 'index.php?option=com_kbi&amp;task=query&amp;format=raw';
+	var params = document.getElementById('arb' + id).innerHTML;
+	// element, do ktereho prijde vysledek
+	var result = $('arb_result' + id);
+
+	result.empty().addClass('ajax-loading');
+	result.removeClass('hidden');
+	result.addEvent('click', function(){result.removeClass('ajax-loading');});
+
+	var myAjax = new Ajax(service_url,
+		{
+			method : 'post',
+			update : result,
+			data : {
+				source : 3, // typ_zdroje (Lucene, Ontopia..)
+				query : 2, // typ_dotazu (vyjimka, podobnost)
+				params : params, // arBuilder = vygenerovane XML
+				xslt : 2, // nic
+			},
+			onComplete : function(response) {
+				result.removeClass('ajax-loading');
+			}
+		}
+	).request();
+
+	return false;
+}
