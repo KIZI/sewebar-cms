0ce4cc690101c7b916f0c075cebc10b2baccf8c4
balhar.jakub@gmail.com <balhar.jakub@gmail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2011-03-16 11:42:31 +0000

    Uprava kvuli zavorkam

--- trunk/tools/ARDesigner/testGetInfo.php
+++ trunk/tools/ARDesigner/testGetInfo.php
@@ -9,14 +9,14 @@
     
     $DD = "XML/datadescription.xml";
     $FL = "XML/featurelistQueryByAr.xml";
+    $ER = null;
     //$FL = "XML/featurelist1.xml";
     //$ER = "XML/taskSetting.xml";
     //$ER = "XML/associationRules.xml";
     //$ER = "XML/arQuery.xml";
     //$ER = "testSelenium/test30Rules/associationRules.xml";
     //$ER = "testSelenium/test30ElementsRule/associationRules.xml";
-    $ER = null;
-
+    
     //$DD = "testSelenium/testSpeed200attributes/datadescription.xml";
     //$FL = "testSelenium/testSpeed200attributes/featurelist.xml";
     //$ER = "testSelenium/testSpeed200attributes/associationRules.xml";
