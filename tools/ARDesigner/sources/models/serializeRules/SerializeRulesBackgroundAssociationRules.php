f94ac43061c6365af6f0d5112301af0383e3360f
balhar.jakub <balhar.jakub@gmail.com>
2011-03-23 12:06:21 +0000

    Correction: DataDescription can be given as a String and not just file.

--- tools/ARDesigner/sources/models/serializeRules/SerializeRulesBackgroundAssociationRules.php
+++ tools/ARDesigner/sources/models/serializeRules/SerializeRulesBackgroundAssociationRules.php
@@ -177,7 +177,11 @@ class SerializeRulesBackgroundAssociationRules extends AncestorSerializeRules {
         $domDD1 = $_SESSION["ARBuilder_domDataDescr"];
         // load XML
         $domDD = new DomDocument();
-        $domDD->load($domDD1);
+        if (file_exists($domDD1)) {
+            $domDD->load($domDD1);
+        } else {
+            $domDD->loadXML($domDD1);
+        }
         // get <Dictionary>
         $fields = $domDD->getElementsByTagName("Dictionary");
 
--- tools/ARDesigner/sources/models/serializeRules/SerializeRulesQueryByAR.php
+++ tools/ARDesigner/sources/models/serializeRules/SerializeRulesQueryByAR.php
@@ -179,7 +179,11 @@ class SerializeRulesQueryByAR extends AncestorSerializeRules {
         $domDD1 = $_SESSION["ARBuilder_domDataDescr"];
         // load XML
         $domDD = new DomDocument();
-        $domDD->load($domDD1);
+        if (file_exists($domDD1)) {
+            $domDD->load($domDD1);
+        } else {
+            $domDD->loadXML($domDD1);
+        }
         // get <Dictionary>
         $fields = $domDD->getElementsByTagName("Dictionary");
 
