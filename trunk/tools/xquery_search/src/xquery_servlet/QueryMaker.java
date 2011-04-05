2447f6ed1cdc00b1a5ca8919c7e0ca77ffc3f207
marekt.87@gmail.com <marekt.87@gmail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2011-04-05 20:30:38 +0000

    XQuery - new handling for query (in development - new progress made) - method for query preparation ready

--- trunk/tools/xquery_search/src/xquery_servlet/QueryMaker.java
+++ trunk/tools/xquery_search/src/xquery_servlet/QueryMaker.java
@@ -16,47 +16,64 @@ import org.xml.sax.SAXException;
  * @author Tomas Marek
  */
 public class QueryMaker {
-
-    public QueryMaker() {
+    String containerName;
+    public QueryMaker(String containerName) {
+        this.containerName = containerName;
     }
 
     public String makeXPath(InputStream xmlQuery){
         String output = "";
-        String output_xml = "";
         try {
-                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-                DocumentBuilder db = dbf.newDocumentBuilder();
-                Document doc = db.parse(xmlQuery);
-                doc.getDocumentElement().normalize();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xmlQuery);
+            doc.getDocumentElement().normalize();
 
-                NodeList fieldList = doc.getElementsByTagName("Field");
-                /*for (int i = 0; i < bbaList.getLength(); i++) {
-                    Element bbaElement = (Element)bbaList.item(i);
-                    NodeList bbaChildList = bbaElement.getChildNodes();*/
-                    for (int j = 0; j < fieldList.getLength() ; j++) {
-                        Element fieldChildElement = (Element)fieldList.item(j);
-                        if (fieldChildElement.getAttribute("dictionary").equals("DataDictionary")) {
-                            output_xml += "<Field>";
-                            NodeList namesList = fieldChildElement.getElementsByTagName("Name");
-                            NodeList typesList = fieldChildElement.getElementsByTagName("Type");
-                            NodeList catsList = fieldChildElement.getElementsByTagName("Category");
-                            for (int k = 0; k < namesList.getLength(); k++) {
-                                Element nameElement = (Element)namesList.item(k);
-                                NodeList names = nameElement.getChildNodes();
-                                Node name = names.item(0);
-                                output_xml += "<Name>" + name.getNodeValue() + "</Name>";
-                            }
-                            for (int k = 0; k < catsList.getLength(); k++) {
-                                Element catElement = (Element)catsList.item(k);
-                                NodeList cats = catElement.getChildNodes();
-                                Node cat = cats.item(0);
-                                output_xml += "<Category>" + cat.getNodeValue() + "</Category>";
-                            }
-                            output_xml += "</Field>";
-                        }
+            int i = 0;
+            output += "collection(\""+containerName+"\")/PMML/AssociationRule[";
+            NodeList fieldList = doc.getElementsByTagName("Field");
+            for (int j = 0; j < fieldList.getLength() ; j++) {
+                Element fieldChildElement = (Element)fieldList.item(j);
+                if (fieldChildElement.getAttribute("dictionary").equals("DataDictionary")) {
+                    NodeList namesList = fieldChildElement.getElementsByTagName("Name");
+                    NodeList typesList = fieldChildElement.getElementsByTagName("Type");
+                    NodeList catsList = fieldChildElement.getElementsByTagName("Category");
+                    NodeList intsList = fieldChildElement.getElementsByTagName("Interval");
+                    if (i > 0) {
+                        output += " and ";
                     }
-                output += output_xml;
-                //}
+                    output += "(";
+
+                    Element nameElement = (Element)namesList.item(0);
+                    NodeList names = nameElement.getChildNodes();
+                    Node name = names.item(0);
+                    Element typeElement = (Element)typesList.item(0);
+                    NodeList types = typeElement.getChildNodes();
+                    Node type = types.item(0);
+                    if (catsList.getLength() > 0 && intsList.getLength() == 0) {
+                        for (int k = 0; k < (catsList.getLength()); k++) {
+                            Element catElement = (Element)catsList.item(k);
+                            NodeList cats = catElement.getChildNodes();
+                            Node cat = cats.item(0);
+                            String connective = "";
+                            if(k > 0) {
+                                if (type.getNodeValue().equals("At least one from listed")){
+                                    connective = " or ";
+                                } else {
+                                    connective = " and ";
+                                }
+                            }
+                            output += connective+".//BBA[DDName=\""+name.getNodeValue()+"\"]/DDValue=\""+cat.getNodeValue()+"\"";
+                        }
+                    } else if (catsList.getLength() == 0 && intsList.getLength() > 0) {
+                        Element intElement = (Element)intsList.item(0);
+                        output += ".//BBA[DDName=\""+name.getNodeValue()+"\" and Interval/@left = "+intElement.getAttribute("left")+"  and Interval/@right = "+intElement.getAttribute("right")+"]";
+                    }
+                    output += ")";
+                    i++;
+                }
+            }
+            output += "]/Text";
         } catch (SAXException ex) {
             //Logger.getLogger(QueryMaker.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
