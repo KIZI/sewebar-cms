abec1fa51226dc3d68bcfff3b596cdc59c6fc111
hazucha.andrej <hazucha.andrej@gmail.com>
2011-04-21 09:11:17 +0000

    SEWEBAR Dev Updater

--- joomla/www/update.php
+++ joomla/www/update.php
@@ -1,7 +1,17 @@
 <?php
-$path = '..';
-echo 'Updating "<i>'. realpath($path) . '"</i>...<br>';
-
-echo exec("svn update $path 2>&1");
-
+$paths = array('..', 'ardesigner');
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>SEWEBAR Dev Updater</title>
+</head>
+<body>
+<?php foreach($paths as $path): ?>
+	Updating <i><?php print realpath($path) ?></i>...<br>
+	<?php print exec("svn update $path 2>&1") ?>
+	<br /><br />
+<?php endforeach; ?>
+</body>
+</html>
