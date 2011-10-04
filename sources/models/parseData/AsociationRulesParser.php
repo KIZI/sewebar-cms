69afd592596898da2aff4906b793d25d443f4669
skrabal <skrabal@drosera.cz>
2011-10-04 15:49:55 +0000

    - bugfix - AssociationRules parsing (brackets)

--- sources/models/parseData/AsociationRulesParser.php
+++ sources/models/parseData/AsociationRulesParser.php
@@ -1,4 +1,4 @@
-<?php                                                      
+<?php
 /**
  * This class represents one DBA
  *
@@ -6,81 +6,84 @@
  * @version 1.0
  */
 class DBA{
-    /**
-     * It creates instance of this class based on params
-     *
-     * @param <DomNode> $dbaNode Node representing one DBA
-     * @param <Array> $elements array containing elements of DBA
-     */
-    function  __construct($dbaNode, $elements) {
-        $this->elements = $elements;
-        $this->refs = array();
-        $this->connective = "";
-        $this->id = Utils::getAttribute($dbaNode, "id");
-        $utils = new Utils();
+  /**
+   * It creates instance of this class based on params
+   *
+   * @param <DomNode> $dbaNode Node representing one DBA
+   * @param <Array> $elements array containing elements of DBA
+   */
+  function  __construct($dbaNode, &$elements) {
+    $this->elements = &$elements;
+    $this->refs = array();
+    $this->connective = "";
+    $this->id = Utils::getAttribute($dbaNode, "id");
+    $this->isAntOrCons = false;
+    $utils = new Utils();
 
-        $dbas = $dbaNode->childNodes;
-        $connective = $utils->getAttribute($dbaNode, "connective");
-        if($connective == "Negation"){
-            $this->connective = "NEG";
-        }
-        else if($connective == "Conjunction"){
-            $this->connective = "AND";
-        }
-        else if($connective == "Disjunction"){
-            $this->connective = "OR";
-        }
-        foreach ($dbas as $dba) {
-            if($dba->nodeName == "BARef"){
-                $this->refs[] = $dba->nodeValue;
-            }
-        }
+    $dbas = $dbaNode->childNodes;
+    $connective = $utils->getAttribute($dbaNode, "connective");
+    if($connective == "Negation"){
+      $this->connective = "NEG";
+    }
+    else if($connective == "Conjunction"){
+      $this->connective = "AND";
+    }
+    else if($connective == "Disjunction"){
+      $this->connective = "OR";
+    }
+    foreach ($dbas as $dba) {
+      if($dba->nodeName == "BARef"){
+        $this->refs[] = $dba->nodeValue;
+      }
+    }
+  }
+
+  /**
+   * It is simple getter returning id
+   *
+   * @return <integer> id
+   */
+  public function getId(){
+    return $this->id;
+  }
+
+  /**
+   * It creates JSON from this Object
+   *
+   * @return <String> JSON representing this Object
+   */
+  public function toJSON(){
+    // Pro vsechny refs
+    $elements = array();
+
+    if (count($this->refs) > 1 && !$this->isAntOrCons) {
+      $elements[] = Utils::getBoolean("(", "lbrac");
+    }
+    
+    foreach ($this->refs as $k => $ref) {
+      if($this->connective == "NEG"){
+        $elements[] = Utils::getBoolean($this->connective, strtolower($this->connective));
+      }
+
+      // Proved toJSON a pole ktere vrati spoj.
+      // Je jeste potreba nejprve podle id DBA ziskat
+      $allElements = $this->elements[$ref]->toJSON();
+      for($el = 0; $el < count($allElements); $el++){
+        $elements[] = $allElements[$el];
+      }
+      
+      // dopln pole o boolean operator
+      if($k + 1 < count($this->refs) && $this->connective != "NEG"){
+        $elements[] = Utils::getBoolean($this->connective, strtolower($this->connective));
+      }
     }
 
-    /**
-     * It is simple getter returning id
-     *
-     * @return <integer> id
-     */
-    public function getId(){
-        return $this->id;
+    if (count($this->refs) > 1 && !$this->isAntOrCons) {
+      $elements[] = Utils::getBoolean(")", "rbrac");
     }
 
-    /**
-     * It creates JSON from this Object
-     *
-     * @return <String> JSON representing this Object
-     */
-    public function toJSON(){
-        // Pro vsechny refs
-        $elements = array();
-        
-        if (count($this->refs) > 1) {
-          $elements[] = Utils::getBoolean("(", "lbrac");
-        }
-        
-        for($actualEl = 0; $actualEl < count($this->refs);$actualEl++){
-            if($this->connective == "NEG"){
-                $elements[] = Utils::getBoolean($this->connective, strtolower($this->connective));
-            }
-            // Proved toJSON a pole ktere vrati spoj.
-            // Je jeste potreba nejprve podle id DBA ziskat
-            $allElements = $this->elements[$this->refs[$actualEl]]->toJSON();
-            for($el = 0; $el < count($allElements); $el++){
-                $elements[] = $allElements[$el];
-            }
-            // dopln pole o boolean operator
-            if($actualEl+1 < count($this->refs) && $this->connective != "NEG"){
-                $elements[] = Utils::getBoolean($this->connective, strtolower($this->connective));
-            }
-        }
-        
-        if (count($this->refs) > 1) {
-          $elements[] = Utils::getBoolean(")", "rbrac");
-        }
-        
-        return $elements;
-    }
+    return $elements;
+  }
 }
 
 /**
@@ -90,60 +93,60 @@ class DBA{
  * @version 1.0
  */
 class BBA{
-    /**
-     * It creates instance of this class based on params
-     *
-     * @param <DomNode> $bbaNode Node representing one BBA
-     */
-    public function  __construct($bbaNode) {
-        $this->fieldRef = "";
-        $this->catRef = array();
+  /**
+   * It creates instance of this class based on params
+   *
+   * @param <DomNode> $bbaNode Node representing one BBA
+   */
+  public function  __construct($bbaNode) {
+    $this->fieldRef = "";
+    $this->catRef = array();
 
-        $bbaChildren = $bbaNode->childNodes;
-        foreach ($bbaChildren as $bbaChild) {
-            if($bbaChild->nodeName == "FieldRef"){
-                $this->fieldRef = $bbaChild->nodeValue;
-            }
-            if($bbaChild->nodeName == "CatRef"){
-                $this->catRef[] = $bbaChild->nodeValue;
-            }
-        }
-        $utils = new Utils();
-        $this->id = $utils->getAttribute($bbaNode,"id");
+    $bbaChildren = $bbaNode->childNodes;
+    foreach ($bbaChildren as $bbaChild) {
+      if($bbaChild->nodeName == "FieldRef"){
+        $this->fieldRef = $bbaChild->nodeValue;
+      }
+      if($bbaChild->nodeName == "CatRef"){
+        $this->catRef[] = $bbaChild->nodeValue;
+      }
+    }
+    $utils = new Utils();
+    $this->id = $utils->getAttribute($bbaNode,"id");
+  }
+
+  /**
+   * It is simple getter returning id
+   *
+   * @return <integer> id
+   */
+  public function getId(){
+    return $this->id;
+  }
+
+  /**
+   * It creates JSON from this Object
+   *
+   * @return <String> JSON representing this Object
+   */
+  public function toJSON(){
+    $element = array();
+    $element['name'] = $this->fieldRef;
+    $element['type'] = "attr";
+    $element['category'] = "One category";
+
+    $fields = array();
+    for($actualField = 0; $actualField < sizeof($this->catRef); $actualField++){
+      $field = array();
+      $field['name'] = "category";
+      $field['value'] = $this->catRef[$actualField];
+
+      $fields[] = $field;
     }
 
-    /**
-     * It is simple getter returning id
-     *
-     * @return <integer> id
-     */
-    public function getId(){
-        return $this->id;
-    }
-
-    /**
-     * It creates JSON from this Object
-     *
-     * @return <String> JSON representing this Object
-     */
-    public function toJSON(){
-        $element = array();
-        $element['name'] = $this->fieldRef;
-        $element['type'] = "attr";
-        $element['category'] = "One category";
-
-        $fields = array();
-        for($actualField = 0; $actualField < sizeof($this->catRef); $actualField++){
-            $field = array();
-            $field['name'] = "category";
-            $field['value'] = $this->catRef[$actualField];
-
-            $fields[] = $field;
-        }
-
-        $element['fields'] = $fields;
-        return array($element);
-    }
+    $element['fields'] = $fields;
+    return array($element);
+  }
 }
 
 /**
@@ -154,68 +157,73 @@ class BBA{
  * @version 2.0
  */
 class AsociationRule{
-    /**
-     * It creates AsociationRule based on the parameters
-     *
-     * @param <DomNode> $asociationRuleNode Node representing one rule
-     * @param <DomDocument> $domER DomDocument representing XML containing rules
-     */
-    function  __construct($asociationRuleNode, $domER) {
-        $this->antecedent = Utils::getAttribute($asociationRuleNode, "antecedent");
-        $this->consequent = Utils::getAttribute($asociationRuleNode, "consequent");
-        $this->interestMeasures = array();
-        $elements = $asociationRuleNode->childNodes;
-        foreach ($elements as $element){
-            if($element->nodeName == "IMValue"){
-              $this->interestMeasures[] = Utils::getIm($element, "name");
-            }
-        }    
-        
-        $this->elements = array();
-        
-        // init BBAs
-        $elements = $domER->getElementsByTagName('BBA');
-        foreach ($elements as $element){
-            $bba = new BBA($element);
-            $this->elements[$bba->getId()] = $bba;
-        }
-        
-        // init DBAs
-        $elements = $domER->getElementsByTagName('DBA');
-        foreach ($elements as $element){
-            $dba = new DBA($element, $this->elements);
-            $this->elements[$dba->getId()] = $dba;
-        }
+  /**
+   * It creates AsociationRule based on the parameters
+   *
+   * @param <DomNode> $asociationRuleNode Node representing one rule
+   * @param <DomDocument> $domER DomDocument representing XML containing rules
+   */
+  function  __construct($asociationRuleNode, $domER) {
+    $this->antecedent = Utils::getAttribute($asociationRuleNode, "antecedent");
+    $this->consequent = Utils::getAttribute($asociationRuleNode, "consequent");
+
+    $this->interestMeasures = array();
+    $elements = $asociationRuleNode->childNodes;
+    foreach ($elements as $element){
+      if($element->nodeName == "IMValue"){
+        $this->interestMeasures[] = Utils::getIm($element, "name");
+      }
     }
 
-    /**
-     * It gets all elements and creates JSON from them
-     * 
-     * @return <String> JSON representing Rules
-     */
-    public function toJSON(){
-        $arrayOfElements = array();
-        $antJson = $this->elements[$this->antecedent]->toJSON();
-        foreach ($antJson as $element){
-            $arrayOfElements[] = $element;
-        }
-        
-        foreach($this->interestMeasures as $interestMeasure) {
-          $im = array();
-          $im['name'] = $interestMeasure['name'];
-          $im['type'] = 'oper';
-          $im['category'] = '';
-          $im['fields'] = array();
-          $im['fields'] = array(array('name' => 'prahovaHodnota', 'value' => $interestMeasure['value']));
-          $arrayOfElements[] = $im;
-        }
-        
-        $consJson = $this->elements[$this->consequent]->toJSON();
-        foreach ($consJson as $element){
-            $arrayOfElements[] = $element;
-        }
-        return $arrayOfElements;
+    $this->elements = array();
+
+    // init BBAs
+    $elements = $domER->getElementsByTagName('BBA');
+    foreach ($elements as $element){
+      $bba = new BBA($element);
+      $this->elements[$bba->getId()] = $bba;
     }
+
+    // init DBAs
+    $elements = $domER->getElementsByTagName('DBA');
+    foreach ($elements as $element){
+      $dba = new DBA($element, $this->elements);
+      if ($dba->getId() == $this->antecedent || $dba->getId() == $this->consequent) { $dba->isAntOrCons = true; }
+      $this->elements[$dba->getId()] = $dba;
+    }
+  }
+
+  /**
+   * It gets all elements and creates JSON from them
+   *
+   * @return <String> JSON representing Rules
+   */
+  public function toJSON(){
+    $arrayOfElements = array();
+    $antJson = $this->elements[$this->antecedent]->toJSON();
+    foreach ($antJson as $element){
+      $arrayOfElements[] = $element;
+    }
+
+    foreach($this->interestMeasures as $interestMeasure) {
+      $im = array();
+      $im['name'] = $interestMeasure['name'];
+      $im['type'] = 'oper';
+      $im['category'] = '';
+      $im['fields'] = array();
+      $im['fields'] = array(array('name' => 'prahovaHodnota', 'value' => $interestMeasure['value']));
+      $arrayOfElements[] = $im;
+    }
+    
+    if ($this->elements[$this->consequent] !== null) {
+      $consJson = $this->elements[$this->consequent]->toJSON();
+      foreach ($consJson as $element){
+        $arrayOfElements[] = $element;
+      }
+    }
+    
+    return $arrayOfElements;
+  }
 }
 
 ?>
