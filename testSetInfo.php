f1c1e589cde06240dc9b95d52c8de04adb966bb5
skrabal <skrabal@drosera.cz>
2011-10-14 12:28:09 +0000

    - code reformat

--- testSetInfo.php
+++ testSetInfo.php
@@ -1,22 +1,23 @@
-<?php      
-    require_once('sources/models/JSON.php');
-    require_once('sources/models/serializeRules/AncestorSerializeRules.php');
-    require_once('sources/models/serializeRules/SerializeRulesBackgroundAssociationRules.php');
-    require_once('sources/models/serializeRules/SerializeRulesARQuery.php');
-    require_once('sources/models/serializeRules/SerializeRulesTaskSetting.php');
-    require_once('sources/models/serializeRules/SerializeRulesQueryByAR.php');
-    require_once('sources/models/Utils.php');
+<?php
 
-    session_start();
-    
-    $toSolve = isset($_POST['data']) ? $_POST['data'] : $_GET['data'];
-    //$toSolve = $_GET['data'];
-    $toSolve = str_replace("\\\"", "\"", $toSolve);
-    //echo $toSolve;
+require_once('sources/models/JSON.php');
+require_once('sources/models/serializeRules/AncestorSerializeRules.php');
+require_once('sources/models/serializeRules/SerializeRulesBackgroundAssociationRules.php');
+require_once('sources/models/serializeRules/SerializeRulesARQuery.php');
+require_once('sources/models/serializeRules/SerializeRulesTaskSetting.php');
+require_once('sources/models/serializeRules/SerializeRulesQueryByAR.php');
+require_once('sources/models/Utils.php');
 
-    //$sr = new SerializeRulesBackgroundAssociationRules();
-    //$sr = new SerializeRulesARQuery();
-    //$sr = new SerializeRulesQueryByAR();
-    $sr = new SerializeRulesTaskSetting();
-    echo $sr->serializeRules($toSolve);
+session_start();
+
+$toSolve = isset($_POST['data']) ? $_POST['data'] : $_GET['data'];
+$toSolve = str_replace("\\\"", "\"", $toSolve);
+//echo $toSolve;
+
+//$sr = new SerializeRulesBackgroundAssociationRules();
+//$sr = new SerializeRulesARQuery();
+//$sr = new SerializeRulesQueryByAR();
+$sr = new SerializeRulesTaskSetting();
+echo $sr->serializeRules($toSolve);
+
 ?>
