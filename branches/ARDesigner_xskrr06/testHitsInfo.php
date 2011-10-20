1a1b6d08816aca426733bd815f3f63aee5cf199f
skrabal@drosera.cz <skrabal@drosera.cz@5df13726-ab30-94f0-ca3f-5d815ade5363>
2011-10-20 15:45:22 +0000

    - debug

--- branches/ARDesigner_xskrr06/testHitsInfo.php
+++ branches/ARDesigner_xskrr06/testHitsInfo.php
@@ -67,7 +67,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 
 // ziskani vysledku tasku z LISpMiner-a
-$response = curl_exec($ch);
+echo $response = curl_exec($ch); die;
 $info = curl_getinfo($ch);
 curl_close($ch);
 
