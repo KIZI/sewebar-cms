9067884fa8868fadd3a1e09f5b6ccb2b03f0bf07
hazucha.andrej <hazucha.andrej@5df13726-ab30-94f0-ca3f-5d815ade5363>
2011-11-20 10:21:15 +0000

    Sewebar Connect - making API more transparent

--- tools/SewebarConnect/Import.ashx.cs
+++ tools/SewebarConnect/Import.ashx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Web;
-using System.Xml.Linq;
 
 namespace SewebarWeb
 {
@@ -10,12 +9,13 @@ namespace SewebarWeb
 		public override void ProcessRequest(HttpContext context)
 		{
 			base.ProcessRequest(context);
-			var response = new XElement("response");
+			
 			var miner = this.Miner;
 			var content = context.Request["content"];
 			var dataFolder = String.Format("{1}/xml/{0}", miner != null ? miner.Id : String.Empty, AppDomain.CurrentDomain.GetData("DataDirectory"));
-
-			context.Response.ContentType = "text/xml";
+			var response = new API.ImportResponse {
+				Id = context.Session.SessionID
+			};
 
 			if (miner != null && content != null)
 			{
@@ -33,15 +33,13 @@ namespace SewebarWeb
 				importer.Input = input;
 				importer.Execute();
 
-				response.Value = String.Format("Imported {0} to {1}", importer.Input, importer.Dsn);
+				response.Message = String.Format("Imported {0} to {1}", importer.Input, importer.Dsn);
+				response.Status = Status.success;
 			}
+			
+			context.Response.ContentType = "text/xml";
 
-			response.SetAttributeValue("id", context.Session.SessionID);
-
-			new XDocument(
-				new XDeclaration("1.0", "utf-8", "yes"),
-				response
-			).Save(context.Response.OutputStream);
+			response.ToXml().Save(context.Response.OutputStream);
 		}
 	}
 }
