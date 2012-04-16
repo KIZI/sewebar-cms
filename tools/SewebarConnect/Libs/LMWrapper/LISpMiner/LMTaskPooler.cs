b2f36f8f3ea16d5848fcc27bfa4e3f3f40183848
hazucha.andrej <hazucha.andrej@5df13726-ab30-94f0-ca3f-5d815ade5363>
2012-04-16 05:50:35 +0000

    SewebarConnect - added support for LMTaskPooler.

---
+++ tools/SewebarConnect/Libs/LMWrapper/LISpMiner/LMTaskPooler.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using log4net;
+
+namespace LMWrapper.LISpMiner
+{
+	public class LMTaskPooler : Executable
+	{
+		private static readonly ILog Log = LogManager.GetLogger(typeof(LMTaskPooler));
+
+		private Process _process;
+
+		private readonly Stopwatch _stopwatch;
+
+		protected Process Process
+		{
+			get
+			{
+				if (this._process == null)
+				{
+					this._process = new Process
+					                	{
+					                		EnableRaisingEvents = true,
+					                		StartInfo = new ProcessStartInfo
+					                		            	{
+					                		            		FileName = String.Format("{0}/{1}", this.LMPath, this.ApplicationName),
+					                		            		Arguments = this.Arguments
+					                		            	},
+					                	};
+
+					this._process.Exited += new EventHandler(ProcessExited);
+				}
+
+				return this._process;
+			}
+		}
+
+		/// <summary>
+		/// /DSN:[data-source-name] ... data source name of metabase (if the data source name contains spaces, the whole /DSN paramater has to be enclosed in quatations mark, e.g. "/DSN:LM Barbora MB")
+		/// </summary>
+		public override string Dsn { get; set; }
+
+		/// <summary>
+		/// /TaskID [TaskID]		... TaskID of selected task
+		/// </summary>
+		public string TaskId { get; set; }
+
+		/// <summary>
+		/// /TaskName:[TaskName]		... Task.Name of the selected task
+		/// </summary>
+		public string TaskName { get; set; }
+
+		/// <summary>
+		/// /TimeOut [sec]			... optional: time-out in seconds (approx.) after generation (excluding initialisation) is automatically interrupted
+		/// </summary>
+		public int? TimeOut { get; set; }
+
+		public int KeepAlive { get; set; }
+
+		public override string Arguments
+		{
+			get
+			{
+				var arguments = new StringBuilder("");
+
+				if (!String.IsNullOrEmpty(this.Dsn))
+				{
+					arguments.AppendFormat("/DSN {0} ", this.Dsn);
+				}
+
+				// /TaskID <TaskID>
+				if (!String.IsNullOrEmpty(this.TaskId))
+				{
+					arguments.AppendFormat("/TaskID:{0} ", this.TaskId);
+				}
+
+				// /TaskName <TaskName>
+				if (!String.IsNullOrEmpty(this.TaskName))
+				{
+					arguments.AppendFormat("\"/TaskName:{0}\" ", this.TaskName);
+				}
+
+				// /TimeOut <sec>
+				if (this.TimeOut != null)
+				{
+					arguments.AppendFormat("/TimeOut {0} ", this.TimeOut);
+				}
+
+				// /Quiet
+				if (this.Quiet)
+				{
+					arguments.Append("/Quiet ");
+				}
+
+				// /NoProgress
+				if (this.NoProgress)
+				{
+					arguments.Append("/NoProgress ");
+				}
+
+				// /AppLog
+				if (!String.IsNullOrEmpty(this.AppLog))
+				{
+					arguments.AppendFormat("\"/AppLog:{0}\"", this.AppLog);
+				}
+
+				return arguments.ToString().Trim();
+			}
+		}
+
+		public LMTaskPooler()
+			: base()
+		{
+			this.ApplicationName = "LMTaskPooler.exe";
+			this._stopwatch = new Stopwatch();
+			this.AppLog = String.Format("{0}-{1}.dat", "_AppLog_LMTaskPooler", Guid.NewGuid());
+			//this.TimeOut = 10;
+		}
+
+		protected override void Run()
+		{
+			if (this.Status != ExecutableStatus.Ready) return;
+
+			this.Status = ExecutableStatus.Running;
+
+			Log.Debug(String.Format("Launching: {0} {1}", this.ApplicationName, this.Arguments));
+
+			this._stopwatch.Start();
+
+			this.Process.Start();
+			// wait a little
+			this.Process.WaitForExit(5*1000);
+		}
+
+		private void ProcessExited(object sender, EventArgs e)
+		{
+			this._stopwatch.Stop();
+			this.Status = ExecutableStatus.Ready;
+
+			Log.InfoFormat("Result generation finished in {2} ms: {0} {1}", this.ApplicationName, this.Arguments,
+			               this._stopwatch.Elapsed);
+
+			this.Process.Close();
+			this._process = null;
+		}
+
+		public void Stop()
+		{
+			if (!this.Process.HasExited)
+			{
+				Log.Debug("stopping / killing");
+
+				if (!this.Process.CloseMainWindow())
+				{
+					this.Stop();
+				}
+			}
+		}
+	}
+}
---
+++ tools/SewebarConnect/TaskPooler.ashx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using LMWrapper;
+using LMWrapper.LISpMiner;
+using log4net;
+
+namespace SewebarWeb
+{
+	public class TaskPooler : API.HttpHandlerSession
+	{
+		private static readonly ILog Log = LogManager.GetLogger(typeof(Task));
+
+		private API.TaskResponse Response { get; set; }
+
+		private API.TaskRequest Request { get; set; }
+
+		protected string GetStatus(string xmlPath)
+		{
+			var document = XDocument.Load(xmlPath);
+
+			return document.XPathEvaluate("//@taskState") as string;
+		}
+
+		public override void ProcessRequest(HttpContext context)
+		{
+			base.ProcessRequest(context);
+
+			this.Request = new API.TaskRequest(this.Miner, context);
+
+			this.Response = new API.TaskResponse(context);
+
+			if (this.Miner != null && this.Request.Task != null)
+			{
+				var status = "Not generated";
+
+				var exporter = this.Miner.Exporter;
+				exporter.Output = String.Format("{0}/results_{1}_{2:yyyyMMdd-Hmmss}.xml", this.Request.DataFolder,
+												this.Request.TaskFileName, DateTime.Now);
+				exporter.Template = String.Format(@"{0}\Sewebar\Template\ARDExport.LM.Template.txt", exporter.LMPath);
+				//exporter.Template = String.Format(@"{0}\Sewebar\Template\4ftMiner.Task.PMML.Template.txt", exporter.LMPath);
+				exporter.Alias = String.Format(@"{0}\Sewebar\Template\LM.PMML.Alias.ARD.txt", exporter.LMPath);
+				exporter.TaskName = this.Request.TaskName;
+
+				try
+				{
+					// try to export results
+					exporter.Execute();
+					status = this.GetStatus(exporter.Output);
+				}
+				catch (LISpMinerException ex)
+				{
+					// task was never imported - does not exists. Therefore we need to import at first.
+					Log.Debug(ex);
+
+					// import task
+					var importer = this.Miner.Importer;
+					importer.Input = this.Request.TaskName;
+					importer.Alias = String.Format(@"{0}\Sewebar\Template\LM.PMML.Alias.ARD.txt", importer.LMPath);
+					importer.Execute();
+				}
+
+				switch (status)
+				{
+					// * Not Generated (po zadání úlohy nebo změně v zadání)
+					case "Not generated":
+					// * Interrupted (přerušena -- buď kvůli time-outu nebo max počtu hypotéz)
+					case "Interrupted":
+						// run task - generate results
+						if (this.Miner.LMTaskPooler.Status == ExecutableStatus.Ready)
+						{
+							var task4FtGen = this.Miner.Task4FtGen;
+							task4FtGen.TaskName = this.Request.TaskName;
+							task4FtGen.Execute();
+
+							// run export once again to refresh results and status
+							if (status != "Interrupted")
+								exporter.Execute();
+						}
+						else
+						{
+							Log.Debug("Waiting for result generation");
+						}
+						break;
+					// * Running (běží generování)
+					case "Running":
+					// * Waiting (čeká na spuštění -- pro TaskPooler, zatím neimplementováno)
+					case "Waiting":
+						this.Miner.Task4FtGen.KeepAlive = 10;
+						break;
+					// * Solved (úspěšně dokončena)
+					case "Solved":
+					case "Finnished":
+					default:
+						break;
+				}
+
+				this.Response.OutputFilePath = exporter.Output;
+			}
+			else
+			{
+				this.Response.Status = Status.failure;
+				this.Response.Message = "No LISpMiner instance or task defined";
+			}
+
+			this.Response.Write();
+		}
+	}
+}
