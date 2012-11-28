adf23c3cd9c6dfb4c1dd705e26b2887a34b2a805
stanislav.vojir <stanislav.vojir@gmail.com>
2012-11-28 14:45:53 +0000

    gc185

--- joomla/www/components/com_dbconnect/views/izishowpreprocessing_intervalenumeration/tmpl/default.php
+++ joomla/www/components/com_dbconnect/views/izishowpreprocessing_intervalenumeration/tmpl/default.php
@@ -18,9 +18,54 @@
                 <strong>'.htmlspecialchars((string)@$this->preprocessingHint->Name).'</strong>
               </td>
             </tr>
-          </table>
+            <tr>
+              <td colspan="2">&nbsp;</td>
+            <tr>
+            <tr>
+              <td>'.JText::_('CREATED_LAST_MODIFIED').'</td>
+              <td>';
+              echo date(JText::_(DATETIME_FORMAT),strtotime(@$this->preprocessingHint->Created->Timestamp));
+              if ((string)@$this->preprocessingHint->Created->Timestamp!=(string)@$this->preprocessingHint->LastModified->Timestamp){
+                echo ' / '.date(JText::_(DATETIME_FORMAT),strtotime($this->preprocessingHint->LastModified->Timestamp));  
+              }
+  echo       '</td>
+            </tr>
+          </table>';
+          
+  if (count(@$this->preprocessingHint->IntervalEnumeration->IntervalBin)>0){
+    foreach ($this->preprocessingHint->IntervalEnumeration->IntervalBin as $intervalBin){
+    	echo '<div class="groupDiv">';
+      echo '<h3>'.@$intervalBin->Name.'</h3>';
+      if (count($intervalBin->Interval)>0){
+        echo '<ul class="binValuesUl">';
+        foreach ($intervalBin->Interval as $interval){
+        	echo '<li>';
+          if (substr(@$interval['closure'],0,4)=='open'){
+            echo JText::_('INTERVAL_LEFT_OPEN');
+            $closureRight=substr(@$interval['closure'],4);
+          }else{
+            echo JText::_('INTERVAL_LEFT_CLOSED');
+            $closureRight=substr(@$interval['closure'],6);
+          }  
+          echo $interval['leftMargin'].' ; '.$interval['rightMargin'];
+          if ($closureRight=='Closed'){
+            echo JText::_('INTERVAL_RIGHT_CLOSED');
+          }else{
+            echo JText::_('INTERVAL_RIGHT_OPEN');
+          }
+          echo '</li>';
+        }
+        echo '</ul>';
+      }else{
+        echo '<div>'.JText::_('NO_INTERVALS_FOUND').'</div>';
+      }  
+      echo '</div>';
+    }
+  }else{
+    echo '<div class="warnDiv">'.JText::_('NO_BINS_FOUND').'</div>';
+  }              
 
-          <div class="formActionsDiv">
+  echo '  <div class="formActionsDiv">
             <a href="'.JRoute::_('index.php?option=com_dbconnect&controller=izi&tmpl=component&task=selectPreprocessingHint&maName='.urlencode($this->maName).'&formatName='.urlencode($this->formatName).'&pmmlName='.urlencode($this->pmmlName).'&preprocessingName='.urlencode((string)$this->preprocessingHint->Name).'&taskId='.urlencode($this->taskId)).'" class="button">'.JText::_('SELECT_PREPROCESSING').'</a>
             <a href="'.JRoute::_('index.php?option=com_dbconnect&controller=izi&tmpl=component&task=editPreprocessingHint_nominalEnumeration&maName='.urlencode($this->maName).'&formatName='.urlencode($this->formatName).'&pmmlName='.urlencode($this->pmmlName).'&oldPhName='.urlencode((string)$this->preprocessingHint->Name).'&taskId='.urlencode($this->taskId)).'" class="button">'.JText::_('EDIT_PREPROCESSING').'</a>
           </div>
