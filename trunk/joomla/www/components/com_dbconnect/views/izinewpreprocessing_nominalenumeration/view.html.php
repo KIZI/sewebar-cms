771a151eac58b05ff6a792158b95e2f3cb8182e7
stanislav.vojir@gmail.com <stanislav.vojir@gmail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2012-12-12 16:39:55 +0000

    oddělení JS nominalenumeration.js z views/izinewpreprocessing_nominalenumeration/tmpl/default.php

--- trunk/joomla/www/components/com_dbconnect/views/izinewpreprocessing_nominalenumeration/view.html.php
+++ trunk/joomla/www/components/com_dbconnect/views/izinewpreprocessing_nominalenumeration/view.html.php
@@ -13,6 +13,26 @@ class iziViewIziNewPreprocessing_NominalEnumeration extends JView
 		$document = & JFactory::getDocument();
 		$document->setTitle( JText::_('NEW_PREPROCESSING') );
     
+    $document->addScriptDeclaration("var lang=".json_encode(array(
+                                                              'BLANK_BIN_NAME'=>JText::_('BLANK_BIN_NAME'),
+                                                              'BIN_NAME_HAS_BEEN_USED_FOR_OTHER_BIN'=>JText::_('BIN_NAME_HAS_BEEN_USED_FOR_OTHER_BIN'),
+                                                              'DELETE_GROUP'=>JText::_('DELETE_GROUP'),
+                                                              'GROUP_NAME'=>JText::_('GROUP_NAME'),
+                                                              'DELETE'=>JText::_('DELETE'),
+                                                              'NO_ITEMS_TO_ADD'=>JText::_('NO_ITEMS_TO_ADD'),
+                                                              'VALUE_TO_ADD'=>JText::_('VALUE_TO_ADD'),
+                                                              'ADD_TO_GROUP'=>JText::_('ADD_TO_GROUP'),
+                                                              'CANCEL'=>JText::_('CANCEL'),
+                                                              'REALLY_DELETE_SELECTED_GROUP'=>JText::_('REALLY_DELETE_SELECTED_GROUP'),
+                                                              'ADD_ITEM'=>JText::_('ADD_ITEM'),
+                                                              'DELETE'=>JText::_('DELETE'),
+                                                              'NOT_SUBMITTED_VALUE_WARNING'=>JText::_('NOT_SUBMITTED_VALUE_WARNING'),
+                                                              'NO_GROUPED_VALUES_FOUND'=>JText::_('NO_GROUPED_VALUES_FOUND'),
+                                                              'BLANK_NOMINAL_GROUPS_WARNING'=>JText::_('BLANK_NOMINAL_GROUPS_WARNING')
+                                                            )));
+                                                            
+    JHtml::script('nominalenumeration.js','media/com_dbconnect/js/');
+    
     parent::display();		
   }
 }
