0c6f08e1f0dc74b17744783199c8d587a016b45f
hazucha.andrej <hazucha.andrej@5df13726-ab30-94f0-ca3f-5d815ade5363>
2013-03-08 07:40:18 +0000

    SewebarConnect - SewebarKey project initial.

---
+++ trunk/tools/SewebarConnect/SewebarKey/Miner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SewebarKey
+{
+	public class Miner
+	{
+		public virtual Guid Id { get; set; }
+
+		public virtual string Path { get; set; }
+
+		public virtual User Owner { get; set; }
+	}
+}
