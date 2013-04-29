10936cc6f658555a58e5e330b3b78f0926e2733b
hazucha.andrej <hazucha.andrej@5df13726-ab30-94f0-ca3f-5d815ade5363>
2013-04-29 09:41:35 +0000

    get rid of czech characters

---
+++ tools/SewebarConnect/Sources/SewebarConnect/App_Start/SecurityConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using SewebarConnect.Security;
+
+namespace SewebarConnect
+{
+    public class SecurityConfig
+    {
+        public static void ConfigureGlobal(HttpConfiguration globalConfig)
+        {
+            System.Diagnostics.Debugger.Break();
+
+            globalConfig.MessageHandlers.Add(new AuthenticationHandler(CreateConfiguration()));
+            globalConfig.Filters.Add(new SecurityExceptionFilter());
+        }
+
+        private static AuthenticationConfiguration CreateConfiguration()
+        {
+            var config = new AuthenticationConfiguration
+            {
+                DefaultAuthenticationScheme = "Basic",
+            };
+
+            config.AddBasicAuthentication((userName, password) => userName == password);
+
+            return config;
+        }
+    }
+}
