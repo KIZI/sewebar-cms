7c5748d44cdaef639109a93267df8a3dc9fc78e0
hazucha.andrej@googlemail.com <hazucha.andrej@googlemail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2013-07-05 12:50:10 +0000

    SewebarConnect   - console compatible with http://docs.sewebarconnect.apiary.io/

--- trunk/tools/SewebarConnect/Sources/SewebarConnect/Scripts/console.js
+++ trunk/tools/SewebarConnect/Sources/SewebarConnect/Scripts/console.js
@@ -1,33 +1,104 @@
 ﻿$(document).ready(function () {
+	function utf8ToB64(str) {
+		return window.btoa(unescape(encodeURIComponent(str)));
+	}
+
+	function b64ToUtf8(str) {
+		return decodeURIComponent(escape(window.atob(str)));
+	}
+
 	function encodeCredentials(user, password) {
 		var tok = user + ':' + password,
-			hash = btoa(tok);
+			hash = utf8ToB64(tok);
 		
 		return "Basic " + hash;
 	}
 
+	function createHeaders() {
+		var username = $('user_username').val(),
+			password = $('user_password').val();
+
+		if (username) {
+			return {
+				"Authorization": encodeCredentials(username, password)
+			};
+		}
+
+		return null;
+	}
+
+	function xml2string(xml) {
+		return (new XMLSerializer()).serializeToString(xml);
+	}
+	
+	function appendXmlElement(root, parent, name, value) {
+		var tag = root.createElement(name);
+
+		tag.appendChild(root.createTextNode(value));
+
+		parent.appendChild(tag);
+
+		return tag;
+	}
+
+	function setStatusClass(textarea, status) {
+		textarea.removeClass();
+		textarea.addClass(status);
+	}
+	
+	$('h2').click(function () {
+		var cls = $(this).attr('class'),
+			form = 'form.' + cls;
+
+		$(form).toggle(500);
+	});
+
 	// registration
 	$('button[name="reg"]').click(function () {
+		var data = $.parseXML("<RegistrationRequest/>"),
+			database = data.createElement('Connection'),
+			databaseType = $('#reg_type').val(),
+			metabase = $('#reg_metabase').val(),
+			metabaseType = $('#reg_metabase_type').val(),
+			m;
+		
+		data.firstChild.appendChild(database);
+
+		database.setAttribute('type', databaseType);
+		
+		if (databaseType == 'MySQL') {
+			appendXmlElement(data, database, 'Server', $('#reg_server').val());
+			appendXmlElement(data, database, 'Database', $('#reg_database').val());
+			appendXmlElement(data, database, 'Username', $('#reg_username').val());
+			appendXmlElement(data, database, 'Password', $('#reg_password').val());
+		} else {
+			appendXmlElement(data, database, 'File', $('#reg_server').val());
+		}
+		
+		if (metabase) {
+			m = data.createElement('Metabase');
+			
+			m.setAttribute('type', metabaseType);
+			appendXmlElement(data, m, 'File', metabase);
+
+			data.firstChild.appendChild(m);
+		}
+
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: 'miners',
-			data: {
-				type: $('#reg_type').val(),
-				metabase: $('#reg_metabase').val(),
-				server: $('#reg_server').val(),
-				database: $('#reg_database').val(),
-				username: $('#reg_username').val(),
-				password: $('#reg_password').val()
-			},
-			dataType: 'text',
-			headers: { "Authorization": encodeCredentials($('user_username'), $('user_password')) },
-			complete: function (data) {
+			data: xml2string(data),
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
+			complete: function (response) {
 				var textarea = $('#reg_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
-				textarea.val(data.responseText);
+				setStatusClass(textarea, response.status == 200 ? 'success' : 'failure');
+				textarea.val(response.responseText);
 			}
-		}).done(function (data) {
-			var xml = jQuery.parseXML(data),
+		}).done(function (response) {
+			var xml = response, // jQuery.parseXML(response),
 				id = $(xml).contents().attr('id');
 
 			if (id) {
@@ -44,13 +115,16 @@
 	// import
 	$('button[name="import"]').click(function () {
 		$.ajax({
-			type: "PATCH",
-			url: 'miners/' + $('#import_id').val(),
+			type: 'PUT',
+			url: ['miners/', $('#import_id').val(), '/DataDictionary'].join(''),
 			data: $('#import_request').val(),
-			dataType: 'text',
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
 			complete: function (data) {
 				var textarea = $('#import_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
@@ -61,13 +135,16 @@
 	// task pooler
 	$('button[name="task_pool"]').click(function () {
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: 'miners/' + $('#import_id').val() + '/tasks/task' + '?alias=' + $('#task_alias').val() + '&template=' + $('#task_template').val(),
 			data: $('#task_request').val(),
-			dataType: 'text',
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
 			complete: function (data) {
 				var textarea = $('#task_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
@@ -78,13 +155,16 @@
 	// proc pooler
 	$('button[name="proc_pool"]').click(function () {
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: 'miners/' + $('#import_id').val() + '/tasks/proc' + '?alias=' + $('#task_alias').val() + '&template=' + $('#task_template').val(),
 			data: $('#task_request').val(),
-			dataType: 'text',
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
 			complete: function (data) {
 				var textarea = $('#task_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
@@ -95,13 +175,16 @@
 	// grid pooler
 	$('button[name="grid_pool"]').click(function () {
 		$.ajax({
-			type: "POST",
+			type: 'POST',
 			url: 'miners/' + $('#import_id').val() + '/tasks/grid' + '?alias=' + $('#task_alias').val() + '&template=' + $('#task_template').val(),
 			data: $('#task_request').val(),
-			dataType: 'text',
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
 			complete: function (data) {
 				var textarea = $('#task_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
@@ -111,14 +194,26 @@
 
 	// task_pool_cancel
 	$('button[name="task_pool_cancel"]').click(function () {
+		var data = $.parseXML('<CancelationRequest></CancelationRequest>'),
+			task = $('#cancel_task').val();
+
 		$.ajax({
-			type: "POST",
-			url: 'miners/' + $('#cancel_id').val() + '/tasks/task/' + $('#cancel_task').val() + '/cancel',
-			dataType: 'text',
-			complete: function (data) {
+			type: 'PUT',
+			url: [
+				'miners/',
+				$('#cancel_id').val(),
+				'/tasks/task/',
+				task
+			].join(''),
+			data: xml2string(data),
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
+			complete: function (response) {
 				var textarea = $('#cancel_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
-				textarea.val(data.responseText);
+				setStatusClass(textarea, response.status == 200 ? 'success' : 'failure');
+				textarea.val(response.responseText);
 			}
 		});
 
@@ -127,14 +222,26 @@
 
 	// proc_pool_cancel
 	$('button[name="proc_pool_cancel"]').click(function () {
+		var data = $.parseXML('<CancelationRequest></CancelationRequest>'),
+			task = $('#cancel_task').val();
+
 		$.ajax({
-			type: "POST",
-			url: 'miners/' + $('#cancel_id').val() + '/tasks/proc/' + $('#cancel_task').val() + '/cancel',
-			dataType: 'text',
-			complete: function (data) {
+			type: 'PUT',
+			url: [
+				'miners/',
+				$('#cancel_id').val(),
+				'/tasks/proc/',
+				task
+			].join(''),
+			contentType: 'text/xml',
+			dataType: 'xml',
+			data: xml2string(data),
+			cache: false,
+			headers: createHeaders(),
+			complete: function (response) {
 				var textarea = $('#cancel_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
-				textarea.val(data.responseText);
+				setStatusClass(textarea, response.status == 200 ? 'success' : 'failure');
+				textarea.val(response.responseText);
 			}
 		});
 
@@ -143,13 +250,53 @@
 
 	// grid_pool_cancel
 	$('button[name="grid_pool_cancel"]').click(function () {
+		var data = $.parseXML('<CancelationRequest></CancelationRequest>'),
+			task = $('#cancel_task').val();
+
 		$.ajax({
-			type: "POST",
-			url: 'miners/' + $('#cancel_id').val() + '/tasks/grid/' + $('#cancel_task').val() + '/cancel',
-			dataType: 'text',
-			complete: function (data) {
+			type: 'PUT',
+			url: [
+				'miners/',
+				$('#cancel_id').val(),
+				'/tasks/grid/',
+				task
+			].join(''),
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			data: xml2string(data),
+			headers: createHeaders(),
+			complete: function(response) {
 				var textarea = $('#cancel_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, response.status == 200 ? 'success' : 'failure');
+				textarea.val(response.responseText);
+			}
+		});
+
+		return false;
+	});
+
+	// export task
+	$('button[name="b_export_task"]').click(function () {
+		$.ajax({
+			type: 'GET',
+			url: [
+				'miners/',
+				$('#export_id').val(),
+				'/tasks/',
+				$('#export_task').val()
+			].join(''),
+			data: {
+				alias: $('#export_alias').val(),
+				template: $('#export_template').val()
+			},
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
+			complete: function (data) {
+				var textarea = $('#export_response');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
@@ -157,35 +304,26 @@
 		return false;
 	});
 
-	// Exporter
-	$('button[name="export"]').click(function () {
+	// export dd
+	$('button[name="b_export_dd"]').click(function () {
 		$.ajax({
-			type: "GET",
-			url: 'miners/' + $('#cancel_id').val() + '/dictionary',
+			type: 'GET',
+			url: 'miners/' + $('#export_id').val() + '/DataDictionary',
 			data: {
 				matrix: $('#export_matrix').val(),
 				template: $('#export_template').val()
 			},
-			dataType: 'text',
+			contentType: 'text/xml',
+			dataType: 'xml',
+			cache: false,
+			headers: createHeaders(),
 			complete: function (data) {
 				var textarea = $('#export_response');
-				setStatusClass(textarea, data.status == 500 ? 'failure' : 'success');
+				setStatusClass(textarea, data.status == 200 ? 'success' : 'failure');
 				textarea.val(data.responseText);
 			}
 		});
 
 		return false;
 	});
-
-	$('h2').click(function () {
-		var cls = $(this).attr('class'),
-			form = 'form.' + cls;
-
-		$(form).toggle(500);
-	});
 });
-
-function setStatusClass(textarea, status) {
-	textarea.removeClass();
-	textarea.addClass(status);
-}
