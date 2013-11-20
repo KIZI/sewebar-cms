1a45b13b633f7ebd1fa0f7df36859b34f4ed150d
stanislav.vojir@gmail.com <stanislav.vojir@gmail.com@5df13726-ab30-94f0-ca3f-5d815ade5363>
2013-11-20 15:49:22 +0000

    konfigurovatelnost

--- trunk/joomla25/www/components/com_dbconnect/controllers/iziController.php
+++ trunk/joomla25/www/components/com_dbconnect/controllers/iziController.php
@@ -1224,15 +1224,11 @@ class IziController extends JController{
    *  Akce pro vyvolání úpravy preprocessing hintu
    */
   public function newReportArticle(){
-    $categoryId=JRequest::getInt('catid',-1);//možnost zadání konkrétní kategorie pro článek
-    if ($categoryId==-1){ 
-      //TODO výběr kategorie, do které se mají články přidávat
-      //TODO načtení výchozí kategorie pro analytické zprávy
-      $categoryId=2;
-     /*
-      JError::raiseError(500,JText::_('FORBIDDEN'));
-      return;                */
-    }
+    /** @var $dataModel dbconnectModelConfig */
+    $configModel=&$this->getModel('Config','dbconnectModel');
+    $categoryId=$configModel->loadConfig('new_report_category');
+
+
     $title=JRequest::getString('title','');
     $todo=JRequest::getString('todo','');
     $kbiId=JRequest::getInt('kbi',0);
