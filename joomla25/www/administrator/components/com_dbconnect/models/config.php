f726e3bfd935a3404af1eb1f55ed0ed5cb50e782
stanislav.vojir <stanislav.vojir@gmail.com>
2013-11-20 15:22:45 +0000

    konfigurovatelnost

--- joomla25/www/administrator/components/com_dbconnect/models/config.php
+++ joomla25/www/administrator/components/com_dbconnect/models/config.php
@@ -7,7 +7,50 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class dbconnectModelCongif extends JModel{
+class dbconnectModelConfig extends JModel{
 
+  /**
+   *   Funkce pro uložení konkrétního nastavení
+   */
+  public function saveConfig($name,$value){
+    $db=$this->getDBO();
+    $user =& JFactory::getUser();
+    $db->setQuery('UPDATE #__dbconnect_config SET value="'.$db->getEscaped($value).'" WHERE `name`="'.$db->getEscaped($name).'" LIMIT 1;');
+    return $db->query();
+  }
 
+  /**
+   *   Funkce pro načtení konkrétního nastavení
+   */
+  public function loadConfig($name){
+    $db=$this->getDBO();
+    $db->setQuery('SELECT value FROM #__dbconnect_config WHERE `name`="'.$db->getEscaped($name).'" LIMIT 1;');
+    $object=$db->loadObject();
+    return $object->value;
+  }
+
+  /**
+   * Funkce vracející přehled všech nastavení
+   * @return array
+   */
+  public function getConfigsArr(){
+    $db=$this->getDBO();
+    $db->setQuery('SELECT * FROM #__dbconnect_config;');
+    $list=$db->loadObjectList();
+    $result=array();
+    foreach ($list as $item){
+      $result[$item->name]=$item->value;
+    }
+    return $result;
+  }
+
+  /**
+   * Funkce vracející přehled všech nastavení
+   * @return array
+   */
+  public function getConfigsList(){
+    $db=$this->getDBO();
+    $db->setQuery('SELECT * FROM #__dbconnect_config;');
+    return $db->loadObjectList();
+  }
 }
